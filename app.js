7bfdd3f28a85413c4beba3a2b80b35e20bb14b5a
Yashmalhotra98 <Blackhacker1998@Gmail.com>
2021-09-16 00:12:14 +0530

    import Javascript

---
+++ app.js
@@ -0,0 +1,15 @@
+// console.log('script is working from a different file');
+
+// alert("This script Works");
+
+// NOTE:- Alert is Not a good way to show messages and such things works on questionable websites.
+
+
+// Input
+var username = prompt('What is your Username ?');
+
+// Processing
+var welcomeMessage = 'this script works!! ' + username;
+
+// Output
+alert(welcomeMessage);
